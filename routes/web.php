d7a807582f7ad463db5e44259155ef683f4fa55e
gabrielpereira1603 <pereiragabrieldev@gmail,com>
2025-03-05 19:52:31 -0300

    criando sidemenu

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,6 @@ Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
-
 
 
 require __DIR__.'/auth.php';
