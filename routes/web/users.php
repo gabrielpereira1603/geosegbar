3cde8444723d6aaf6a15bb4545b340f2244021ef
gabrielpereira1603 <pereiragabrieldev@gmail,com>
2025-03-13 00:31:42 -0300

    ajustando responsividade login

--- routes/web/users.php
+++ routes/web/users.php
@@ -6,8 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::middleware(RequiredLoginMiddleware::class)->group(function () {
-
+    Route::get('users', HomeUsers::class)
+        ->name('users');
 });
 
-Route::get('users', HomeUsers::class)
-    ->name('users');
